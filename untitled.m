c0a20ce45dcb101fe77e4d014289f41331252696
MaeveMeehan <meehanm24@students.ecu.edu>
2026-09-07 17:30:59 -0400

    Add files via upload

---
+++ untitled.m
@@ -0,0 +1,9 @@
+%Maeve Meehan September 5, 2026%
+%In this assignment I will be creating a code to play Tic Tac Toe%
+% I will begin the program with a displaying a welcome message and
+% prompting the user if they'd like to play a game%
+%If the user would like to play a game, it will display a game board%
+disp('Welcome to Tic Tac Toe')
+disp('Would you like to play a game?')
+%If the user responds "Yes" the board will be displayed, If "No" the game
+%will not begin%
